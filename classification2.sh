982a83c12e7785a96acc4f3121c5e3c40d8a36b5
Zixind <zixin@uchicago.edu>
2023-05-08 10:23:46 -0500

    Adding some accuracies

--- classification2.sh
+++ classification2.sh
@@ -7,13 +7,13 @@
 # batch_size=512
 
 # Slurm arguments
-node=e[001-002],f[002-003]   #only a002 working
-partition=yuxinchen-contrib
-mem=48G
-jobname=GLISTER_MNIST
+node=c001    #only a002 working
+partition=general
+mem=24G
+jobname=GLISTER_SVHN
 
 # Get the results for the dense network
-srun -w ${node} --gres=gpu:4 -c 32 --ntasks-per-node=1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 classification3.py --acquisition GLISTER --dataset MNIST
+srun -w ${node} --gres=gpu:1 -c 48 --ntasks-per-node=1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 classification3.py --acquisition GLISTER --dataset SVHN
 
 
 # chmod 777 ${results_root}
--- classification3.sh
+++ classification3.sh
@@ -5,9 +5,9 @@
 node=r[001-005]
 partition=yuxinchen-contrib
 mem=48G
-jobname=BADGE_SVHN
+jobname=CoreSet_SVHN
 
-srun -w ${node} --gres=gpu:4 -c 64 --ntasks-per-node=1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 classification3.py --dataset SVHN --acquisition BADGE
+srun -w ${node} --gres=gpu:4 -c 64 --ntasks-per-node=1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 classification3.py --dataset SVHN --acquisition CoreSet
 
 
 
