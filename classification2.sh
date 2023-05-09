db19fe4e68bcaccb50d7ac714b385b7e5fffdff9
Zixind <zixin@uchicago.edu>
2023-05-08 21:25:11 -0500

    Updating accuracies

--- classification2.sh
+++ classification2.sh
@@ -7,13 +7,13 @@
 # batch_size=512
 
 # Slurm arguments
-node=c001    #only a002 working
-partition=general
-mem=24G
-jobname=GLISTER_SVHN
+node=aa[001-002]   #only a002 working
+partition=yuxinchen-contrib
+mem=8G
+jobname=GLISTER_MNIST
 
 # Get the results for the dense network
-srun -w ${node} --gres=gpu:1 -c 48 --ntasks-per-node=1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 classification3.py --acquisition GLISTER --dataset SVHN
+srun -w ${node} --gres=gpu:2 -c 32 --ntasks-per-node=1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 classification3.py --acquisition GLISTER --dataset MNIST
 
 
 # chmod 777 ${results_root}
