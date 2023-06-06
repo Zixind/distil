aef472a7ef576e06a45fc62107ea2b29f08cc5a1
Zixind <zixin@uchicago.edu>
2023-06-06 17:34:50 -0500

    rearranging some losses

--- OT_distance3.sh
+++ OT_distance3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-node=a003
+node=a006
 partition=yuxinchen-contrib
 mem=11G
 jobname=OT_distance_evaluate
 initial=20
 dataset='CIFAR10'
-samplesize=50
+samplesize=20
 ot_distance=1
-net_trained=80
+net_trained=50
 
 srun -w ${node} --gres=gpu:1 -c 64 --ntasks-per-node 1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 OT_distance3.py --Label_Initialize $initial --dataset $dataset --sample_size $samplesize --OT_distance $ot_distance --Net_trained $net_trained
 
