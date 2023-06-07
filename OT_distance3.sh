f5b697c7994867decfcc60c3f006924ebc56931a
Zixind <zixin@uchicago.edu>
2023-06-06 19:06:54 -0500

    Adding some losses

--- OT_distance3.sh
+++ OT_distance3.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-node=a003
+node=a004
 partition=yuxinchen-contrib
 mem=11G
 initial=20
 dataset='CIFAR10'
-samplesize=20
+samplesize=10
 ot_distance=1
 net_trained=50
 jobname=OT_distance_$net_trained_$samplesize
 
 
-srun -w ${node} --gres=gpu:1 -c 64 --ntasks-per-node 1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 OT_distance3.py --Label_Initialize $initial --dataset $dataset --sample_size $samplesize --OT_distance $ot_distance --Net_trained $net_trained
+srun -w ${node} --gres=gpu:1 -c 32 --ntasks-per-node 2 --mem ${mem} -p ${partition} --job-name=${jobname} python3 OT_distance3.py --Label_Initialize $initial --dataset $dataset --sample_size $samplesize --OT_distance $ot_distance --Net_trained $net_trained
 
 
