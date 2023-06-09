089eec9da5812a9b01658815879ede4eff270007
Zixind <zixin@uchicago.edu>
2023-06-09 06:02:29 -0500

    Adding some checkpoints

--- OT_distance.sh
+++ OT_distance.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-node=r004
+node=f003
 partition=yuxinchen-contrib
-mem=48G
+mem=24G
 initial=20
-dataset='SVHN'
-samplesize=50
-ot_distance=0
-ot_distance_only=0
+dataset='CIFAR10'
+samplesize=80
+ot_distance=1
+ot_distance_only=1
 jobname=OT_$ot_distance_$samplesize_$dataset
-epochs=700
+epochs=500
 
 
 
-srun -w ${node} --gres=gpu:1 -c 64 --ntasks-per-node 1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 OT_distance.py --Label_Initialize $initial --dataset $dataset --sample_size $samplesize --OT_distance $ot_distance --Epochs $epochs --OT_distance_only $ot_distance_only
+srun -w ${node} --gres=gpu:1 -c 32 --ntasks-per-node 1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 OT_distance.py --Label_Initialize $initial --dataset $dataset --sample_size $samplesize --OT_distance $ot_distance --Epochs $epochs --OT_distance_only $ot_distance_only
 
 
--- OT_distance3.sh
+++ OT_distance3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-node=a003
+node=c001
 partition=yuxinchen-contrib
-mem=11G
+mem=24G
 initial=20
 dataset='MNIST'
-samplesize=50
+samplesize=100
 ot_distance=1
-ot_distance_only=0
-net_trained=100
+ot_distance_only=1
+net_trained=50
 jobname=OT_distance_$net_trained_$samplesize
 
 
-srun -w ${node} --gres=gpu:1 -c 64 --ntasks-per-node 1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 OT_distance3.py --Label_Initialize $initial --dataset $dataset --sample_size $samplesize --OT_distance $ot_distance --Net_trained $net_trained --OT_distance_only $ot_distance_only
+srun -w ${node} --gres=gpu:1 -c 16 --ntasks-per-node 3 --mem ${mem} -p ${partition} --job-name=${jobname} python3 OT_distance3.py --Label_Initialize $initial --dataset $dataset --sample_size $samplesize --OT_distance $ot_distance --Net_trained $net_trained --OT_distance_only $ot_distance_only
 
 
