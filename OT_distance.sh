e8dd3de1a72cf6af9fe77f8b01bcf630cf1f52e6
Zixind <zixin@uchicago.edu>
2023-06-09 08:16:07 -0500

    Adding losses

--- OT_distance.sh
+++ OT_distance.sh
@@ -4,15 +4,15 @@ node=r004
 partition=yuxinchen-contrib
 mem=24G
 initial=20
-dataset='CIFAR10'
+dataset='SVHN'
 samplesize=80
-ot_distance=1
-ot_distance_only=1
+ot_distance=0
+ot_distance_only=0
 jobname=OT_$ot_distance_$samplesize_$dataset
 epochs=500
 
 
 
-srun -w ${node} --gres=gpu:1 -c 32 --ntasks-per-node 2 --mem ${mem} -p ${partition} --job-name=${jobname} python3 OT_distance.py --Label_Initialize $initial --dataset $dataset --sample_size $samplesize --OT_distance $ot_distance --Epochs $epochs --OT_distance_only $ot_distance_only
+srun -w ${node} --gres=gpu:1 -c 32 --ntasks-per-node 1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 OT_distance.py --Label_Initialize $initial --dataset $dataset --sample_size $samplesize --OT_distance $ot_distance --Epochs $epochs --OT_distance_only $ot_distance_only
 
 
--- OT_distance3.sh
+++ OT_distance3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-node=f002
+node=r[003-005]
 partition=yuxinchen-contrib
 mem=48G
 initial=20
-dataset='SVHN'
+dataset='CIFAR10'
 samplesize=100
 ot_distance=1
-ot_distance_only=1
-net_trained=50
+ot_distance_only=0
+net_trained=20
 jobname=OT_distance_$net_trained_$samplesize
 
 
-srun -w ${node} --gres=gpu:1 -c 32 --ntasks-per-node 1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 OT_distance3.py --Label_Initialize $initial --dataset $dataset --sample_size $samplesize --OT_distance $ot_distance --Net_trained $net_trained --OT_distance_only $ot_distance_only
+srun -w ${node} --gres=gpu:3 -c 32 --ntasks-per-node 1 --mem ${mem} -p ${partition} --job-name=${jobname} python3 OT_distance3.py --Label_Initialize $initial --dataset $dataset --sample_size $samplesize --OT_distance $ot_distance --Net_trained $net_trained --OT_distance_only $ot_distance_only
 
 
